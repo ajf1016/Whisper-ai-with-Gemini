f7d6c45f88b8735f338d1008eee5589d86cab237
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-04-29 01:09:04 +0530

    Intial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import whisper
+import ssl
+import google.generativeai as genai
+import os
+GOOGLE_API_KEY = 'AIzaSyBX_gz-djWYmm165YjD4STUX0l5cgsG9hc'
+ssl._create_default_https_context = ssl._create_unverified_context
+genai.configure(api_key=GOOGLE_API_KEY)
+model = whisper.load_model("base")
+result = model.transcribe("03.mp3")
+model = genai.GenerativeModel('gemini-pro')
+response = model.generate_content("Summarize this text" + result["text"])
+print(response.text)
+print("+======================================================+")
+print(result["text"])
+
+with open("response.txt", "w") as response_file:
+    response_file.write(response.text)
+
+with open("original_text.txt", "w") as original_text_file:
+    original_text_file.write(result["text"])
